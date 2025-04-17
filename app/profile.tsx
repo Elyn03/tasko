787ed5787fd45f1f9fda6799fb706f317499c46c
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-04-18 00:07:53 +0200

    :sparkles: ADD profile page

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,17 +1,144 @@
-import { ThemedText } from "@/components/themed/ThemedText";
-import { ThemedView } from "@/components/themed/ThemedView";
-import { Text, View } from "react-native";
+import {StyleSheet, View, Image, Switch} from "react-native";
+import {useEffect, useState} from "react";
+import {UserAuth} from "@/context/AuthContext";
+import {Colors} from "@/constants/Colors";
+import {supabase} from "@/lib/supabase";
+
+// Navigation
+import {ParamListBase, useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+// Components
+import {ThemedView} from "@/components/themed/ThemedView";
+import {ThemedText} from "@/components/themed/ThemedText";
+import {ThemedButton} from "@/components/themed/ThemedButton";
+import {findUser} from "@/hooks/findUser";
+import {getImageProfile} from "@/hooks/getImageProfile";
+
+type IUser = {
+    id: number;
+    name: string;
+    email: string;
+} | null;
+
 
 export default function ProfileScreen() {
-  return (
-    <ThemedView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ThemedText>YOU CAN VIEW YOUR PROFILE HERE</ThemedText>
-    </ThemedView>
-  );
+
+    const [user, setUser] = useState<IUser>(null)
+    const [isLightMode, setIsLightMode] = useState<boolean>(true)
+    const [enableNotif, setEnableNotif] = useState<boolean>(false)
+    const [duckUrl, setDuckUrl] = useState<string>("https://images.unsplash.com/photo-1578956919791-af7615c94b90?q=80&w=1939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+    const {session, signOut} = UserAuth()
+    const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+    useEffect(() => {
+        getUser().then()
+        getProfile().then()
+    }, []);
+
+    const getUser = async () => {
+        let user = await findUser(session);
+        setUser(user)
+    }
+
+    const getProfile = async () => {
+        let image = await getImageProfile(session);
+        setDuckUrl(image)
+    }
+
+    const logOut = async () => {
+        const { error} = await supabase.auth.signOut();
+        navigation.navigate("SignIn")
+    }
+
+    return (
+        <ThemedView>
+            <View style={styles.container}>
+                <View style={styles.imageProfile}>
+                    <Image
+                        style={styles.image}
+                        source={{
+                            uri: duckUrl,
+                        }}
+                    />
+                </View>
+
+                <View>
+                    <ThemedText style={styles.infoText}>{user ? user.name : "Indéfini"}</ThemedText>
+                    <ThemedText style={styles.infoText}>{user ? user.email : "Indéfini"}</ThemedText>
+                </View>
+
+                <View style={styles.settingsProfile}>
+                    <View style={styles.setting}>
+                        <ThemedText style={styles.settingText}>Light Mode</ThemedText>
+                        <Switch
+                            trackColor={{false: Colors.teal, true: Colors.pinkSalmon}}
+                            thumbColor={isLightMode ? Colors.teal : Colors.pinkSalmon}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={setIsLightMode}
+                            value={isLightMode}
+                        />
+                    </View>
+                    <View style={styles.setting}>
+                        <ThemedText style={styles.settingText}>Notifications</ThemedText>
+                        <Switch
+                            trackColor={{false: Colors.teal, true: Colors.pinkSalmon}}
+                            thumbColor={enableNotif ? Colors.teal : Colors.pinkSalmon}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={setEnableNotif}
+                            value={enableNotif}
+                        /></View>
+                </View>
+
+                <ThemedButton
+                    type={"secondary"}
+                    title={"Se déconnecter"}
+                    onPress={logOut}
+                />
+            </View>
+        </ThemedView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 16,
+    },
+    imageProfile: {
+        height: 150,
+        width: 150,
+        borderRadius: 500,
+        backgroundColor: Colors.pinkSalmon
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        overflow: "hidden",
+        borderRadius: 500
+
+    },
+    infoText: {
+        textAlign: "center"
+    },
+    settingsProfile: {
+        width: "100%",
+        justifyContent: "flex-start"
+    },
+    setting: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 16
+    },
+    settingText: {
+        textAlign: "left"
+    }
+})
