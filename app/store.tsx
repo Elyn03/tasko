be4b66aa6d53c6a626d2d7adb4c97823289a663c
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-04-19 18:25:51 +0200

    :sparkles: ADD front store page

--- app/store.tsx
+++ app/store.tsx
@@ -1,19 +1,124 @@
-import { ThemedText } from "@/components/themed/ThemedText";
-import { ThemedView } from "@/components/themed/ThemedView";
-import { Text, View } from "react-native";
+import {useEffect, useState} from "react";
+import {StyleSheet, View} from "react-native";
+import {supabase} from "@/lib/supabase";
+import {Colors} from "@/constants/Colors";
+
+// Components
+import {ThemedView} from "@/components/themed/ThemedView";
+import {ThemedText} from "@/components/themed/ThemedText";
+import StoreCard from "@/components/StoreCard";
+
+type IData = {
+    id: number;
+    title: string | null;
+    description: string | null;
+    price: number | null;
+    category: string;
+    created_at: string;
+    promotion: number | null;
+};
+
+type IItem = {
+    category: string;
+    items: IData[]
+};
 
 export default function StoreScreen() {
-  return (
-    <ThemedView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ThemedText>
-        HERE THE IN-APP PURCHASE STORE /!\ NOT AVAILABLE YET /!\
-      </ThemedText>
-    </ThemedView>
-  );
+
+    const [items, setItems] = useState<IItem[]>([])
+
+    useEffect(() => {
+        getStoreItem().then()
+    }, []);
+
+    const getStoreItem = async () => {
+        console.log("hllo")
+        const { data, error } = await supabase
+            .from("store_item")
+            .select()
+
+        if (data) {
+            const categories = [...new Set(data.map(item => item.category))];
+            let filterItems = []
+
+            for (let category of categories) {
+                const itemsInCategory: IData[] = data.filter(it => it.category === category)
+                const newItem: IItem = {
+                    category: category,
+                    items: itemsInCategory
+                }
+                filterItems.push(newItem)
+            }
+
+            filterItems.map(it => {
+                it.items.map(da => {
+                    console.log(da)
+                })
+            })
+            setItems(filterItems)
+        }
+    }
+
+    return (
+        <ThemedView>
+            <View style={styles.container}>
+                    {
+                        items.map((item: IItem) => {
+                            return (
+                                <View style={styles.categoryContainer}>
+                                    <View style={styles.categoryTitle}>
+                                        <ThemedText>
+                                            {item.category.toUpperCase()}
+                                        </ThemedText>
+                                    </View>
+
+                                    <View style={styles.category}>
+                                        {
+                                            item.items.map((data: IData) => {
+                                                return (
+                                                    <StoreCard
+                                                        title={data.title}
+                                                        description={data.description}
+                                                        price={data.price}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </View>
+                                </View>
+                            )
+                        })
+                    }
+            </View>
+        </ThemedView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        width: "100%",
+        height: "100%",
+        flexDirection: "column",
+        gap: 32
+    },
+    categoryContainer: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: 8
+    },
+    categoryTitle: {
+        padding: 8,
+        borderRadius: 12,
+        backgroundColor: Colors.salmon
+    },
+    category: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        gap: 12
+    },
+})
---
+++ components/StoreCard.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+import {Colors} from "@/constants/Colors";
+import {ThemedButton} from "@/components/themed/ThemedButton";
+import {ThemedText} from "@/components/themed/ThemedText";
+
+export default function StoreCard(props: any) {
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.image}
+/*
+                source={require('../assets/images/tasko-logo.png')}
+*/
+                source={{
+                    uri: "https://i.pinimg.com/736x/f7/17/7a/f7177a1777d04ef5727890ab4d3e9851.jpg"
+                }}
+            />
+            <View style={styles.textsContainer}>
+                <ThemedText
+                    style={styles.title}
+                    type={"subtitle"}
+                    numberOfLines={2}
+                    ellipsizeMode="tail"
+                >
+                    {props.title}
+                </ThemedText>
+
+                <ThemedText
+                    style={styles.description}
+                    numberOfLines={2}
+                    ellipsizeMode="tail"
+                >
+                    {props.description}
+                </ThemedText>
+
+                <ThemedButton type={"secondary"} title={props.price} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "48%",
+        backgroundColor: Colors.darkTeal,
+        borderRadius: 12,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    image: {
+        padding: 0,
+        margin: 0,
+        height: 100,
+        width: "100%",
+        overflow: "hidden",
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+    },
+    textsContainer: {
+        width: "100%",
+        padding: 16,
+        gap: 8
+    },
+    title: {
+        color: Colors.pinkSalmon,
+        textAlign: "left"
+    },
+    description: {
+        color: Colors.pinkSalmon
+    },
+})
