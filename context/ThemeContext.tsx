82a4253422f2c67a9a1dbcb8b852969e5b3a794b
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-04-20 00:25:08 +0200

    :bug: fix theme hooks

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,36 +1,41 @@
-import {createContext, useState, useEffect, useContext, PropsWithChildren} from "react";
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  PropsWithChildren,
+} from "react";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 type ITheme = "light" | "dark";
 
 const ThemeContext = createContext<{
-    theme: ITheme;
-    toggleTheme: () => void;
+  theme: ITheme;
+  toggleTheme: () => void;
 }>({
-    theme: "light",
-    toggleTheme: () => {},
-})
+  theme: "light",
+  toggleTheme: () => {},
+});
 
-const deviceTheme = useColorScheme() ?? 'light';
+export function ThemeContextProvider({ children }: PropsWithChildren) {
+  const deviceTheme = useColorScheme() ?? "light";
+  const [theme, setTheme] = useState<ITheme>("light");
 
-export function ThemeContextProvider({children}: PropsWithChildren) {
-    const [theme, setTheme] = useState<ITheme>("light")
+  useEffect(() => {
+    setTheme(deviceTheme);
+  }, []);
 
-    useEffect(() => {
-        setTheme(deviceTheme)
-    }, []);
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
-    const toggleTheme = () => {
-        setTheme(theme === "light" ? "dark" : "light")
-    }
-
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
 export function AppTheme() {
-    return useContext(ThemeContext)
+  return useContext(ThemeContext);
 }
