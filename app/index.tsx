1da6f0e1dcfd975fcaa9331abc4d5a2e39fc88a5
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-04-20 22:46:07 +0200

    💥 add tri distance & date

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, Switch, View } from "react-native";
 import { ThemedView } from "@/components/themed/ThemedView";
 import { ThemedText } from "@/components/themed/ThemedText";
 import { useEffect, useState } from "react";
@@ -8,11 +8,15 @@ import { supabase } from "@/lib/supabase";
 import { UserTasks } from "@/context/TaskManager";
 import Task from "@/components/Task";
 import { Task as TaskType } from "@/context/TaskManager";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const sortingTasks: Array<"time" | "distance"> = ["time", "distance"];
 
 export default function HomeScreen() {
   const [session, setSession] = useState<Session | null>(null);
   const [unfinishedTasks, setUnfinishedTasks] = useState<TaskType[]>([]);
   const [finishedTasks, setFinishedTasks] = useState<TaskType[]>([]);
+  const [sortedType, setSortedType] = useState<boolean>(true);
   const { tasks } = UserTasks();
 
   useEffect(() => {
@@ -29,12 +33,34 @@ export default function HomeScreen() {
   useEffect(() => {
     const unfinished = tasks.filter((task) => !task.done);
     const finished = tasks.filter((task) => task.done);
-    console.log("Unfinished tasks:", unfinished);
-    console.log("Finished tasks:", finished);
 
     setUnfinishedTasks(unfinished);
     setFinishedTasks(finished);
   }, [tasks]);
+
+  function toggleSortedType(value: boolean): void | Promise<void> {
+    const sortedByDate = unfinishedTasks
+      .map((task) => ({
+        ...task,
+        time: task.created_at ? -+new Date(task.created_at) : 0,
+      }))
+      .sort(
+        (a, b) =>
+          (a[sortingTasks[+value]] as number) -
+          (b[sortingTasks[+value]] as number)
+      );
+    console.log(
+      "sortedByDate",
+      sortedByDate.map(({ distance }) => distance)
+    );
+    console.log(
+      "created_at",
+      sortedByDate.map(({ time }) => time)
+    );
+
+    setUnfinishedTasks(sortedByDate);
+    setSortedType(value);
+  }
 
   return (
     <ThemedView style={styles.container}>
@@ -42,6 +68,18 @@ export default function HomeScreen() {
         <ThemedText type="title" style={{ paddingTop: 16, paddingLeft: 16 }}>
           Vos tâches en cours :
         </ThemedText>
+        <View style={styles.sortContainer}>
+          <Switch
+            trackColor={{ false: Colors.teal, true: Colors.pinkSalmon }}
+            thumbColor={Colors.salmon}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSortedType}
+            value={sortedType}
+          />
+          <ThemedText>
+            Trier par : {sortedType ? "Distance" : "Date"}
+          </ThemedText>
+        </View>
         <ThemedView
           style={{
             display: "flex",
@@ -95,5 +133,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     zIndex: 1000,
+  },
+
+  sortContainer: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 16,
   },
 });
